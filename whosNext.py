5c91e6e1c317a9a7b9998455849fb5ec62ca1bd5
Charles J McDonald <cjmcdonald42@gmail.com>
2024-12-06 07:36:20 -0500

    create whosNext.py

---
+++ whosNext.py
@@ -0,0 +1,25 @@
+"""
+        package:  whosNext.py
+         author:  Charles J McDonald «cmcdonald@woonsocketschools.com»
+           date:  2024.12.06
+
+    description:  This script will randomly select a name from a list of names.
+
+"""
+
+import random
+
+# PTech 10 Students
+names = ["Evan", "Rey", "Maya", "Jaydriel", "Jaxon", "Ethan", "Kristina"]
+
+# PTech 09 Students
+# names = ["Lucas", "Jvion", "Damian", "Alice", "Nicolas", "Zack", "Jonathan", "Corbin"]
+
+while names:
+    name = random.choice(names)
+    print(f"Our next presenters will be: {name}'s group.")
+    names.remove(name)
+    if names:
+        input("Press Enter to pick the next name...")
+    else:
+        print("All names have been chosen.")
