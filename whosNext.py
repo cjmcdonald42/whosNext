bbd690b592599f5c45337e56ae177ae15fad8d24
Charles J McDonald <cjmcdonald42@gmail.com>
2024-12-06 07:41:17 -0500

    Update whosNext.py

--- whosNext.py
+++ whosNext.py
@@ -3,7 +3,7 @@
          author:  Charles J McDonald «cmcdonald@woonsocketschools.com»
            date:  2024.12.06
 
-    description:  This script will randomly select a name from a list of names.
+    description:  This script will randomly select a name from a list of names until everyone has had a turn.
 
 """
 
